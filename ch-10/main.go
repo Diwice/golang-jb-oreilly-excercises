86554a52695e14b8df21ee00569be440358d738b
Diwice <coldwice@gmail.com>
2026-02-13 21:43:02 +0300

    Ch10/done

--- ch-10/main.go
+++ ch-10/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "fmt"
-// Add accepts two integers
-// Returns int which is a summary of two input integers
+import (
+	"fmt"
+	"golang.org/x/exp/constraints"
+)
+// Number matches any float or integer types
+// Based on [this module's]: golang.org/x/exp/constraints Float and Integer interfaces
+type Number interface{
+	constraints.Float | constraints.Integer
+}
+// Add accepts two Number type variables
+// Returns a Number which is a summary of two input Number(s)
 // [Learn more about addition]: https://www.mathisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
 
