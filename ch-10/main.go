941aec277c96bfd36cce1ac14d7cf14ac2cc09ae
Diwice <coldwice@gmail.com>
2026-02-13 21:30:17 +0300

    Ch10/2-godoc

--- ch-10/main.go
+++ ch-10/main.go
@@ -1,7 +1,9 @@
 package main
 
 import "fmt"
-
+// Add accepts two integers
+// Returns int which is a summary of two input integers
+// [Learn more about addition]: https://www.mathisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
