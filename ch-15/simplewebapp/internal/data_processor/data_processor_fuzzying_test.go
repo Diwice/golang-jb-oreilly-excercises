b8f11d3cce57282b3c6b1389913b282115857d7f
Diwice <coldwice@gmail.com>
2026-02-24 21:33:38 +0300

    Ch15Ex-3/parser fuzzying

--- ch-15/simplewebapp/internal/data_processor/data_processor_fuzzying_test.go
+++ ch-15/simplewebapp/internal/data_processor/data_processor_fuzzying_test.go
@@ -21,3 +21,15 @@ func FuzzDataProcessor(f *testing.F) {
 		close(outChan)
 	})
 }
+// The task itself asks to test parser function, so here it is.
+func FuzzParser(f *testing.F) {
+	tcSeed := []string{"test-1\n+\n1\n1","test-2\n-\n2\n1","test-3\n*\n3\n1","test-4\n/\n4\n2","invalid-test", ""}
+	for _, tc := range tcSeed {
+		f.Add(tc)
+	}
+
+	f.Fuzz(func(t *testing.T, inp string) {
+		inpVal := []byte(inp)
+		_, _ = parser(inpVal)
+	}
+}
